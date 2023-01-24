946483bf5b3aeacb3518edcb1b831c289bddde31
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-01-24 08:54:21 +0545

    dynamic segment :variable pattern

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import "github.com/gin-gonic/gin"
 
 func IndexHanlder(c *gin.Context) {
+	name := c.Params.ByName("name")
 	c.JSON(200, gin.H{
-		"message": "hello world",
+		"message": "hello " + name,
 	})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", IndexHanlder)
+	router.GET("/:name", IndexHanlder)
 	router.Run()
 }
